e05e15c6739ef4dc48c7a9b279189eae311ba280
Priya Gupta <priya.gupta11@ad.infosys.com>
2025-05-04 17:05:50 +0530

    first commit

---
+++ PayRollManagementSystem/src/main/java/com/InnovativeSurvayers/PayRollManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.InnovativeSurvayers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayRollManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayRollManagementSystemApplication.class, args);
+	}
+
+}
---
+++ PayRollManagementSystem/src/test/java/com/InnovativeSurvayers/PayRollManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.InnovativeSurvayers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayRollManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
